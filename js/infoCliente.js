345f690d6119f3866face37483ee5d942a421d22
Diego <zxiteft21@gmail.com>
2024-07-08 23:46:41 -0500

    Third Commit

--- js/infoCliente.js
+++ js/infoCliente.js
@@ -53,3 +53,26 @@ function agregarResumenCarrito() {
 }
 
 agregarResumenCarrito();
+
+const btnFinalizar = document.getElementById("btn-finalizar")
+const nombreForm = document.getElementById("nombre-form")
+const apellidosForm = document.getElementById("apellidos-form")
+const numDocForm = document.getElementById("numero-documento-form")
+const rucForm = document.getElementById("ruc-form")
+const razonSocialForm = document.getElementById("razon-social-form")
+const correoForm = document.getElementById("correo-form")
+const celularForm = document.getElementById("celular-form")
+const metodoPagoForm = document.getElementById("metodo-pago-form")
+btnFinalizar.addEventListener("click", ()=>{
+  alert("Compra Realizada Con Exito!!!!")
+  localStorage.setItem("productos",JSON.stringify([]))
+  nombreForm.value = "";
+  apellidosForm.value = "";
+  numDocForm.value = "";
+  rucForm.value = "";
+  razonSocialForm.value = "";
+  correoForm.value = "";
+  celularForm.value = "";
+  metodoPagoForm.value = "";
+  location.href = "/index.html"
+})
